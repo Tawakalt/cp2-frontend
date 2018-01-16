15cee220d5c5b8ca857c45f2bdf7343a17394740
Andela Developer <tawakalt.olaniyi@andela.com>
2018-01-16 13:31:55 +0100

    chore(app setup): setup basic react app
    fix little errors
    
    FInishes #154132398

---
+++ src/app/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/main.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src="app/img/Tessa-2.svg" alt="Let's Do It!" />
+        <h1>Welcome to Document Management System!!!</h1>
+        <h2 className="welcome">Yeah...welcome</h2>
+      </div>
+    );
+  }
+}
+render(<App />, document.getElementById('app'));
